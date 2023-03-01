e4c2c1b72a9a2415a85d14767691c3318a628cbc
Duy Nguyễn <nguyenduyhd1999@gmail.com>
2023-03-01 15:55:44 +0700

    [Task][05][Complete account view]

---
+++ movie-app/Account/Cells/LogOutTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LogOutTableViewCell.swift
+//  movie-app
+//
+//  Created by Duy Nguyễn on 01/03/2023.
+//
+
+import UIKit
+
+class LogOutTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
